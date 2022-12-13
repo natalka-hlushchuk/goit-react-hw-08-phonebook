d3aeea09878336f0691ff9c43f9e8cb4212c305f
Natalka Hlushchuk <hlushchuk.nataliia@gmail.com>
2022-12-13 10:37:15 +0100

    err

--- src/redux/auth/auth_slice.js
+++ src/redux/auth/auth_slice.js
@@ -8,42 +8,76 @@ export const authSlice = createSlice({
       name: null,
       email: null,
     },
+    isLoading: false,
     token: null,
     isLoggedIn: false,
     isRefreshing: false,
+    error: null,
   },
   extraReducers: builder => {
     builder
-      // .addCase(register.pending, (state, _) => {
-      //   state.contacts.isLoading = true;
-      //   state.contacts.error = '';
-      // })
+      .addCase(register.pending, (state, _) => {
+        state.isLoading = true;
+        state.error = null;
+      })
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
+        state.isLoading = false;
+        state.error = null;
       })
-
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(logIn.pending, (state, _) => {
+        state.isLoading = true;
+        state.error = null;
+      })
       .addCase(logIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
+        state.isLoading = false;
+        state.error = null;
       })
+      .addCase(logIn.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(logOut.pending, (state, _) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+
       .addCase(logOut.fulfilled, (state, action) => {
         state.user = { name: null, email: null };
         state.token = action.payload.token;
         state.isLoggedIn = false;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(logOut.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
       })
       .addCase(currentUser.pending, (state, action) => {
         state.isRefreshing = true;
+        state.isLoading = true;
+        state.error = null;
       })
       .addCase(currentUser.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
+        state.isLoading = false;
+        state.error = null;
       })
       .addCase(currentUser.rejected, (state, action) => {
         state.isRefreshing = false;
+        state.isLoading = false;
+        state.error = action.payload;
       });
   },
 });
